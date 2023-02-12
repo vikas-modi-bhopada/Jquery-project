53d84742549a4e2e418499d27f5771aa908d0888
Vikas Modi <vikasmodi479@gmail.com>
2023-02-12 11:02:19 -0800

    first commt

---
+++ MyJquery.js
@@ -0,0 +1,12 @@
+// $('selector').action()
+$(document).ready(function () {
+    // $('p').click(function () { // It is used to click on all paragrafs
+    //     // console.log('you clicked on p');
+    //     // $('p').hide()  // it is used to hide all para.
+    //      //$(this).hide() // it is used to hide perticular para.
+    //     // $(#id).hide()  
+    //     // $(.class).hide()
+    //      //  $('*').hide() // hide all the elements 
+    //     // $('#para').hide()
+    // })
+})
